a0333ef8779036db055ff94d40d95251b7c60b28
aperushin <aperushin@gmail.com>
2023-07-17 15:47:58 +0300

    Add/alter models
    Add new Location and User models, add fields 'image' and 'category' to Ad.

--- ads/admin.py
+++ ads/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from models import Ad, User, Category, Location
 
-# Register your models here.
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Ad)
+admin.site.register(Category)
+admin.site.register(Location)
--- ads/models.py
+++ ads/models.py
@@ -1,4 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
 
 class Ad(models.Model):
@@ -8,13 +20,44 @@ class Ad(models.Model):
     description = models.CharField(max_length=1000)
     address = models.CharField(max_length=255)
     is_published = models.BooleanField(default=False)
+    image = models.ImageField(null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
 
-class Category(models.Model):
+
+class Location(models.Model):
     name = models.CharField(max_length=255)
+    lat = models.DecimalField(max_digits=10, decimal_places=7)
+    lng = models.DecimalField(max_digits=10, decimal_places=7)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'Локация'
+        verbose_name_plural = 'Локации'
+
+
+class User(AbstractUser):
+    ROLES = (
+        ('member', 'Member'),
+        ('moderator', 'Moderator'),
+        ('admin', 'Admin')
+    )
+
+    role = models.CharField(max_length=9, choices=ROLES, default='member')
+    age = models.PositiveSmallIntegerField()
+    location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.role
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
