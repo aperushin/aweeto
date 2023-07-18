3815aa8688fc844798e8abd081939487cad9ecd8
aperushin <aperushin@gmail.com>
2023-07-18 18:15:15 +0300

    Add default ordering to User model

--- ads/models.py
+++ ads/models.py
@@ -15,8 +15,8 @@ class Category(models.Model):
 
 class Location(models.Model):
     name = models.CharField(max_length=255)
-    lat = models.DecimalField(max_digits=10, decimal_places=7)
-    lng = models.DecimalField(max_digits=10, decimal_places=7)
+    lat = models.DecimalField(max_digits=10, decimal_places=7, null=True)
+    lng = models.DecimalField(max_digits=10, decimal_places=7, null=True)
 
     def __str__(self):
         return self.name
@@ -43,6 +43,7 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+        ordering = ['username']
 
 
 class Ad(models.Model):
