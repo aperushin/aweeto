e952c2f06e70d0a00b7702ad6deaca64b73e9e60
aperushin <aperushin@gmail.com>
2023-07-24 15:50:36 +0300

    Add JWT support

--- avwto/settings.py
+++ avwto/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'ads',
     'users',
 ]
@@ -139,4 +140,7 @@ AUTH_USER_MODEL = "users.User"
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ]
 }
--- users/serializers.py
+++ users/serializers.py
@@ -46,6 +46,8 @@ class UserCreateSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create(**validated_data)
 
+        user.set_password(validated_data['password'])
+
         for location in self._locations:
             location_obj, _ = Location.objects.get_or_create(name=location)
             user.location.add(location_obj)
