fea86723592c779f65f83e9cdb1e2529358b0175
aperushin <aperushin@gmail.com>
2023-07-29 15:54:46 +0300

    Add tests

--- tests/selection/selection_create_test.py
+++ tests/selection/selection_create_test.py
@@ -51,3 +51,32 @@ def test_create_selection_admin_ownerless(client, user, ad, admin_token_and_id):
 
     assert response.data == expected_response
     assert response.status_code == 201
+
+
+@pytest.mark.django_db
+def test_create_selection_user(client, user, ad, user_token_and_id):
+    token, user_id = user_token_and_id
+    other_user_id = user.id
+
+    data = {
+        'name': 'test selection',
+        'items': [ad.pk],
+        'owner': other_user_id,
+    }
+
+    expected_response = {
+        'id': 3,
+        'name': 'test selection',
+        'owner': user_id,
+        'items': [ad.pk],
+    }
+
+    response = client.post(
+        '/selection/',
+        data,
+        format='json',
+        HTTP_AUTHORIZATION=f'Bearer {token}',
+    )
+
+    assert response.data == expected_response
+    assert response.status_code == 201
